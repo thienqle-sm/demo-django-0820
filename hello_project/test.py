0c6dc3d16bb97e206868c0f89f82675b52469208
thien-sm <thien@summitworks.com>
2021-08-20 16:48:27 -0400

    create requirement

--- hello_project/test.py
+++ hello_project/test.py
@@ -8,8 +8,10 @@ class HomePageTests(SimpleTestCase):
         print("Verify admin page")
         response = self.client.get('/admin')
         self.assertEquals(response.status_code,301)
+        # admin -> login
 
     def test_about_page_status_code(self):
         print("Verify not exist about page")
         response = self.client.get('/about')
         self.assertEquals(response.status_code,404)
+          # about 404
