a062b91ac49c933e61883b9ebbea89f4e5de83a2
thien-sm <thien@summitworks.com>
2021-08-20 16:34:26 -0400

    second commit

---
+++ hello_project/test.py
@@ -0,0 +1,15 @@
+
+from django.http import response
+from django.test import SimpleTestCase
+
+class HomePageTests(SimpleTestCase):
+
+    def test_admin_page_status_code(self):
+        print("Verify admin page")
+        response = self.client.get('/admin')
+        self.assertEquals(response.status_code,301)
+
+    def test_about_page_status_code(self):
+        print("Verify not exist about page")
+        response = self.client.get('/about')
+        self.assertEquals(response.status_code,404)
